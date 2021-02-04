7380553edd2d6c9de0fc74b07b139d66981f333f
Harlen Batagelo <harlen.batagelo@gmail.com>
2021-02-03 21:25:55 -0300

    Explicitly specify Ninja generator on Windows

--- build-wasm.bat
+++ build-wasm.bat
@@ -7,4 +7,4 @@ rd /s /q build 2>nul
 mkdir build & cd build
 
 :: Configure and build
-emcmake cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .. & cmake --build . --config %BUILD_TYPE% -- -j %NUMBER_OF_PROCESSORS% & cd ..
+emcmake cmake -G Ninja -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .. & cmake --build . --config %BUILD_TYPE% -- -j %NUMBER_OF_PROCESSORS% & cd ..

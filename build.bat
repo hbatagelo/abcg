f7cd9155ce43ab5f0f0d6388d9b402af8b8cf9a5
Harlen Batagelo <harlen.batagelo@gmail.com>
2021-08-27 18:39:59 -0300

    Set compiler to GCC

--- build.bat
+++ build.bat
@@ -7,7 +7,10 @@ rd /s /q build 2>nul
 mkdir build & cd build
 
 :: Configure
-cmake -GNinja -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+              -DCMAKE_CXX_COMPILER=g++ ^
+              -DCMAKE_C_COMPILER=gcc ^
+              ..
 
 :: Build
 cmake --build . --config %BUILD_TYPE% -- -j %NUMBER_OF_PROCESSORS%

1b198f5afef814fb46ea6fb15c925be9d426637f
Matheus Augusto <mts.013@hotmail.com>
2021-10-19 18:40:46 -0300

    commit inicial

---
+++ examples/game/openglwindow.cpp
@@ -0,0 +1,29 @@
+#include <fmt/core.h>
+
+#include "openglwindow.hpp"
+
+#include <imgui.h>
+
+void OpenGLWindow::initializeGL() {
+  auto windowSettings{getWindowSettings()};
+  fmt::print("Initial window size: {}x{}\n", windowSettings.width,
+             windowSettings.height);
+}
+
+void OpenGLWindow::paintGL() {
+  // Set the clear color
+  abcg::glClearColor(m_clearColor[0], m_clearColor[1], m_clearColor[2],
+                     m_clearColor[3]);
+  // Clear the color buffer
+  abcg::glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void OpenGLWindow::paintUI() {
+
+    // Window begin
+    ImGui::Begin("Game!");
+
+    // Window end
+    ImGui::End();
+
+}

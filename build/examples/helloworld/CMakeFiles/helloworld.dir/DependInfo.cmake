56da2c8c152d34e72d2b9fef9dae6d363b3a4eb8
Eduardo Maciel de Souza <edu.maciel@yahoo.com.br>
2021-10-31 16:28:37 -0300

    Final com Build

---
+++ build/examples/helloworld/CMakeFiles/helloworld.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maciel/Documentos/Comp_grafica/abcg/examples/helloworld/main.cpp" "examples/helloworld/CMakeFiles/helloworld.dir/main.cpp.o" "gcc" "examples/helloworld/CMakeFiles/helloworld.dir/main.cpp.o.d"
+  "/home/maciel/Documentos/Comp_grafica/abcg/examples/helloworld/openglwindow.cpp" "examples/helloworld/CMakeFiles/helloworld.dir/openglwindow.cpp.o" "gcc" "examples/helloworld/CMakeFiles/helloworld.dir/openglwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/maciel/Documentos/Comp_grafica/abcg/build/abcg/CMakeFiles/abcg.dir/DependInfo.cmake"
+  "/home/maciel/Documentos/Comp_grafica/abcg/build/abcg/external/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/home/maciel/Documentos/Comp_grafica/abcg/build/abcg/external/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/examples/helloworld/CMakeFiles/helloworld.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for helloworld.
+# This may be replaced when dependencies are built.

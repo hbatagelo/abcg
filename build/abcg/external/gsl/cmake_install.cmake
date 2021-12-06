276f4bae850915e52980c37c5e29bdec83bbd4c6
Edumacsou <edu.maciel@yahoo.com.br>
2021-12-06 18:34:28 -0300

    compilado e rodando com mudança de cor

--- build/abcg/external/gsl/cmake_install.cmake
+++ build/abcg/external/gsl/cmake_install.cmake
@@ -39,6 +39,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/bin/x86_64-linux-gnu-objdump")
 endif()
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/bin/x86_64-linux-gnu-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
